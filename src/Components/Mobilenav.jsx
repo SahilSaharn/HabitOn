e40cde204936cf16782ffd85c651daf7050ba6eb
Sahil Saharn <sahilsaharn2003@gmail.com>
2023-01-15 01:27:40 +0530

    added some animations in navbar :)

--- src/Components/Mobilenav.jsx
+++ src/Components/Mobilenav.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
 import "../Component_styles/Mobilenav.css"
-import { FaHome , FaCompass , FaUserPlus , FaBars ,FaTimesCircle} from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import { FaHome, FaCompass, FaUserPlus, FaBars, FaTimesCircle } from 'react-icons/fa';
 
 function Mobilenav() {
   return (
-    <div className='mobile-nav-cont'>
-        <div className="links-holder sofi">
-            <a href="/home"> <span> <FaHome/> Home </span> </a>
-            <a href="/joke"> <span> <FaCompass/> Explore </span> </a>
-            <a href="/coke"> <span> <FaUserPlus/> Sign In/Up </span> </a>
-        </div>
-    </div>
+    <motion.div className='mobile-nav-cont'
+      initial={{
+        opacity: 0
+      }}
+      animate={{
+        opacity: 1
+      }}
+      transition={{
+        duration: 0.3
+      }}
+    >
+      <motion.div className="links-holder sofi"
+        initial={{
+          opacity: 0,
+          x: -500,
+        }}
+        animate={{
+          opacity: 1,
+          x: 0
+        }}
+        transition={{
+          duration: 0.5,
+          type: 'spring'
+        }}
+      >
+        <a href="/home"> <span> <FaHome /> Home </span> </a>
+        <a href="/joke"> <span> <FaCompass /> Explore </span> </a>
+        <a href="/coke"> <span> <FaUserPlus /> Sign In/Up </span> </a>
+      </motion.div>
+    </motion.div>
+
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,65 @@
 import React, { useState } from 'react'
 import '../Component_styles/Navbar_styles.css'
+import { easeInOut, motion } from 'framer-motion';
 
-import { FaHome , FaCompass , FaUserPlus , FaBars ,FaTimesCircle} from 'react-icons/fa';
+import { FaHome, FaCompass, FaUserPlus, FaBars, FaTimesCircle } from 'react-icons/fa';
 import Mobilenav from './Mobilenav';
 import '../Component_styles/Navbar_styles.css';
 function Navbar() {
 
-  const [showMobileNav , setShowMobileNav] = useState(false);
+  const [showMobileNav, setShowMobileNav] = useState(false);
   return (<>
     <div className='navbar-cont' >
-        <h2>
-          Habit-O(n)
-        </h2>
-        <div className="links-cont sofi">
-          <a href="/home"> <span> <FaHome/> Home </span> </a>
-          <a href="/joke"> <span> <FaCompass/> Explore </span> </a>
-          <a href="/coke" id='signup-btn'> <span> <FaUserPlus/> Sign In/Up </span> </a>
-        </div>
-        <div className="mobile-nav" onClick={ () => setShowMobileNav( (prev)=> !prev ) } >
-          { showMobileNav ? <FaTimesCircle/> : <FaBars/> }
-        </div>
+      <motion.h2
+
+        initial={{
+          opacity: 0,
+          x: -100
+        }}
+
+        animate={{
+          opacity: 1,
+          x: 0
+        }}
+
+        transition={{
+          duration: 0.6,
+          ease: easeInOut,
+          type: 'spring'
+        }}
+
+      >
+        Habit-O(n)
+      </motion.h2>
+      <div className="links-cont sofi">
+        <a href="/home"> <span> <FaHome /> Home </span> </a>
+        <a href="/joke"> <span> <FaCompass /> Explore </span> </a>
+        <a href="/coke" id='signup-btn'> <span> <FaUserPlus /> Sign In/Up </span> </a>
+      </div>
+
+      <motion.div
+        className="mobile-nav"
+        style={{ color: (showMobileNav) ? '#fb3636' : '#154ab1' }}
+        onClick={() => setShowMobileNav((prev) => !prev)}
+        initial={{
+          rotate: '360deg',
+          x: 50,git 
+        }}
+        animate={{
+          rotate: '0deg',
+          x: 0,
+        }}
+        transition={{
+          duration: 0.5,
+          type: 'spring',
+          ease: easeInOut
+        }}
+      >
+        {showMobileNav ? <FaTimesCircle /> : <FaBars />}
+      </motion.div>
     </div>
-    
-    {showMobileNav && <Mobilenav/>}
+
+    {showMobileNav && <Mobilenav />}
   </>)
 
 }
