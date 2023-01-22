4c969575111fc7020dde57a1130173260db3b4e0
Sahil Saharn <sahilsaharn2003@gmail.com>
2023-01-22 17:40:12 +0530

    created the signup page with input fields and installed two packages loaders for react and axios for post requests

--- src/Components/SignUpPage.jsx
+++ src/Components/SignUpPage.jsx
@@ -1,19 +1,44 @@
 import React , {useContext, useState ,useEffect} from 'react'
 import ErrorContext from '../Contexts/ErrorContext'
-
+import mailImage from "../usedImages/mail_illustartion.png"
 import "../Component_styles/SignUpPage_styles.css"
 
 function SignUpPage() {
     const cont = useContext(ErrorContext);
-    const [x ,setx] = useState(true);
 
-    useEffect( () => {
-        cont.addError("hello this is a error" , false)
-    } ,[x])
+    //funtion and state fr our input field...
+    const [mail ,setMail] = useState("");
+    const getMail = (e) => {
+        setMail(e.target.value)
+    }
+    console.log(mail);
 
-    return (<>
-        
-    </>)
+    const formSubbed= (e) => {
+        e.preventDefault();
+        //now we have to make our post request here and redirect user...
+    }
+
+
+    return (
+    <>
+        <div>
+
+            <div className="signup-card-cont ">
+                <form className="signup-card " onSubmit={formSubbed} >
+                    <div align='center'>
+                        <img draggable='false' id='imageCard' src={mailImage} alt="!oopps" />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="mail">Email</label>
+                        <input type="email" id="mail" spellCheck='false' onChange={getMail} value={mail} />
+                    </div>
+                    <button> Sign Up </button>
+                </form>
+            </div>
+
+        </div>
+    </>
+    )
 }
 
 export default SignUpPage
