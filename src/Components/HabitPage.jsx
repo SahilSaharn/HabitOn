342771fa3602540f834c1033380c7c7935fcdaa2
SahilSaharn <sahilsaharn2003@gmail.com>
2023-02-17 18:01:51 +0530

    added somrthing :)

--- src/Components/HabitPage.jsx
+++ src/Components/HabitPage.jsx
@@ -4,10 +4,18 @@ import userContext from '../Contexts/UserAndThemeContext';
 
 function HabitPage() {
   const {user} = useContext(userContext)
-  const {index} = useParams()
 
+  const index = Number( window.atob( useParams().index ) )
+  //index is user's habit clicked habit in our user habits data
+  console.log(index)
+
+  const habit = user.userHabits[index]
+  //here habit is the habit on which user want to see and now we fetch its data...
+  console.log(habit)
+  
   return (
     <div>
+      <h2>hello i ma habit page {index} </h2>
       <h2>hello i ma habit page {index} </h2>
       <h2>hello i ma habit page {index} </h2>
       <h2>hello i ma habit page {index} </h2>
